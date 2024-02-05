b14d0241c05a2ed0210b5f0d7e0c4c75175a896d
rksanthila <rajesh@santhila.co>
2024-02-05 18:25:55 +0530

    buttons

--- src/views/admin/superadmin/game/demoplay/playcards/ReplayGame.tsx
+++ src/views/admin/superadmin/game/demoplay/playcards/ReplayGame.tsx
@@ -1,6 +1,6 @@
 import { Box, Icon, Img, Text } from '@chakra-ui/react';
 import React from 'react';
-import ReplayBtn from 'assets/img/games/ReplayBtn.png'
+import ReplayBtn from 'assets/img/games/ReplayBtn.png';
 import next from 'assets/img/screens/next.png';
 interface Badge {
   gasId: number;
@@ -12,32 +12,41 @@ const ReplayGame: React.FC<{
   formData: any;
   imageSrc: any;
 }> = ({ formData, imageSrc }) => {
-  
   return (
     <>
       {imageSrc && (
         <>
-          <Box className="thankyou-screen" >
+          <Box className="thankyou-screen">
             <Box className="thankyou-screen-box">
               <Img src={imageSrc} className="bg-Img" />
             </Box>
-            <Box  w={'100%'}
-             display={'flex'}
-             justifyContent={'center'}
-             alignItems={'center'}
-             position={'relative'}
+            <Box
+              w={'100%'}
+              display={'flex'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              position={'relative'}
             >
-            <Box 
+              <Box className="title">
+                <Text fontFamily={'AtlantisContent'} textAlign={'center'}>
+                  Do You Want Replay Again ?
+                </Text>
+              </Box>
+              <Box
                 // onClick={() => getData(data)}
                 position={'fixed'}
                 top={'360px'}
                 w={'40%'}
                 display={'flex'}
                 justifyContent={'space-between'}
-                cursor={'pointer'}
               >
-                <Img src={ReplayBtn} w={'200px'} h={'60px'} />
-                <Img src={next} w={'200px'} h={'60px'} />
+                <Img
+                  src={ReplayBtn}
+                  w={'200px'}
+                  h={'60px'}
+                  cursor={'pointer'}
+                />
+                <Img src={next} w={'200px'} h={'60px'} cursor={'pointer'} />
               </Box>
             </Box>
           </Box>
