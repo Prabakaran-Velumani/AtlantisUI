132f580198c70040591b311d4b193ba02e0d00d4
rksanthila <rajesh@santhila.co>
2024-05-04 09:57:02 +0530

    milestone 3 changes

--- src/views/admin/superadmin/game/components/AddScores.tsx
+++ src/views/admin/superadmin/game/components/AddScores.tsx
@@ -1455,7 +1455,7 @@ const AddScores: React.FC<{
                                         ?.gameAboveDistinctionScoreCongratsMessage
                                         ?.length > maxCharacters2
                                         ? 'red'
-                                        : 'grey'
+                                        : 'gray'
                                     }
                                   >
                                     {maxCharacters2 -
