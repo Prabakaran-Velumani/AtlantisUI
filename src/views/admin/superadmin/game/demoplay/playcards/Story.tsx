29aed7c084030321a1a9fd52d78ec83d194c7b0c
rksanthila <rajesh@santhila.co>
2024-05-23 09:58:21 +0530

    note changes completed

--- src/views/admin/superadmin/game/demoplay/playcards/Story.tsx
+++ src/views/admin/superadmin/game/demoplay/playcards/Story.tsx
@@ -642,7 +642,7 @@ const Story: React.FC<{
                         width={'100%'}
                         mt="20px"
                         position={'fixed'}
-                        top="70%"
+                        top="78%"
                         display={'flex'}
                         justifyContent={'center'}
                       >
