b4f932526e9f57e0112c58e9b3f6efde6e15f815
navinATsanthila <e.navin@santhila.co>
2024-06-07 15:03:13 +0530

    M5 2nd correction update

--- src/config/constant.tsx
+++ src/config/constant.tsx
@@ -3,10 +3,10 @@ if (process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {  
         // BACKEND_SERVER = "http://35.183.46.127:5555"; //old server ip
-        // BACKEND_SERVER = "http://192.168.1.30:5555";
-        BACKEND_SERVER = "http://192.168.1.51:5511";}
+        // BACKEND_SERVER = "http://192.168.1.30:5555"; 
+        BACKEND_SERVER = "http://192.168.1.51:5510";}
 export const API_SERVER = BACKEND_SERVER;
-export const Notelength = 150;
+export const Notelength = 150; 
 export const Dialoglength = 300;
 export const Responselength = 300;
 export const DemoPlayRoutePath = '/game/creator/demoplay/';
